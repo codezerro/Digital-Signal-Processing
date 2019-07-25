1bdffba3172ccc111f315cbdade86631d7cd96fb
CodeZerro <kabircp08@gmail.com>
2019-07-25 14:47:12 +0600

    add some code

---
+++ additon.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+
+int main()
+{
+    int sig[100],sig1[100],n;
+
+    cout<<"Enter number the signal point"<<endl;
+    cin>>n;
+    cout<<"Enter the first signal"<<endl;
+    for(int i=0;i<n;i++){
+        scanf("%d",&sig[i]);
+    }
+    cout<<"Enter the 2nd signal"<<endl;
+    for(int i=0;i<n;i++){
+        scanf("%d",&sig1[i]);
+    }
+    cout<<"the value of addition respect to the input signal"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<sig[i]+sig1[i]<<"  ";
+    }
+    cout<<endl;
+
+    return 0;
+}
